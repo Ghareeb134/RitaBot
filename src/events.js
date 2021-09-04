c26cb7d83c8fd0e9a7db463c09fbd56c5ee79748
Ghareeb134 <88591443+Ghareeb134@users.noreply.github.com>
2021-09-04 14:42:20 -0400

    alpha v3.2
    better

--- src/events.js
+++ src/events.js
@@ -146,7 +146,7 @@ exports.listen = function listen (client)
 
          if (meseg === "!alphaturk")
          {
-            message.channel.send('https://i.imgur.com/zoUqhqS.jpg');
+            message.channel.send('https://i.imgur.com/u3KfEJh.jpg');
          }
 
          if (message.channel.type !== "dm")
