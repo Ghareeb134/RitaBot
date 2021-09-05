43de56cf933b4fdaac2f8ebedcd78a53bf7fe668
Ghareeb134 <88591443+Ghareeb134@users.noreply.github.com>
2021-09-05 08:54:06 -0400

    added !join v1

--- src/events.js
+++ src/events.js
@@ -15,11 +15,54 @@ const botVersion = require("../package.json").version;
 const botCreator = "Collaboration";
 const joinMessage = require("./commands/info_commands/join");
 
+import { Client, VoiceChannel, Intents } from 'discord.js';
+import {
+	joinVoiceChannel,
+	createAudioPlayer,
+	createAudioResource,
+	entersState,
+	StreamType,
+	AudioPlayerStatus,
+	VoiceConnectionStatus,
+} from '@discordjs/voice';
+import { createDiscordJSAdapter } from './adapter';
+
+const player = createAudioPlayer();
+
+
+function playSong() {
+	const resource = createAudioResource('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3', {
+		inputType: StreamType.Arbitrary,
+	});
+
+	player.play(resource);
+
+	return entersState(player, AudioPlayerStatus.Playing, 5e3);
+}
+
+async function connectToChannel(channel: VoiceChannel) {
+	const connection = joinVoiceChannel({
+		channelId: channel.id,
+		guildId: channel.guild.id,
+		adapterCreator: createDiscordJSAdapter(channel),
+	});
+
+	try {
+		await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+		return connection;
+	} catch (error) {
+		connection.destroy();
+		throw error;
+	}
+}
+
+
 // ----------
 // Core Code
 // ----------
 
-exports.listen = function listen (client)
+exports.listen = function listen (
+)
 {
 
    let config = null;
@@ -99,6 +142,14 @@ exports.listen = function listen (client)
             "status": "online"
          });
 
+         console.log('Discord.js client is ready!');
+
+	try {
+		await playSong();
+		console.log('Song is ready to play!');
+	} catch (error) {
+		console.error(error);
+	}
          // ----------------------
          // All shards are online
          // ----------------------
@@ -139,8 +190,26 @@ exports.listen = function listen (client)
 
    client.on(
       "message",
-      (message) =>
+      async (message) =>
       {
+
+         if (!message.guild) return;
+
+	if (message.content === '!join') {
+		const channel = message.member?.voice.channel;
+
+		if (channel) {
+			try {
+				const connection = await connectToChannel(channel);
+				connection.subscribe(player);
+				message.reply('Playing now!');
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+			message.reply('Join a voice channel then try again!');
+		}
+	}
 
          const meseg = message.content.toLowerCase();
 
